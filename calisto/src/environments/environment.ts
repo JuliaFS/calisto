a3638a106a0f5f831576e469937d0be999a6492a
Yulia <yuliya.f.s@gmail.com>
2024-03-21 09:10:10 +0200

    ad firebase auth environment, install @angulare/fire

---
+++ calisto/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyCK0caH1nOAs31L6-xccDiLi2wGFy89Y2g",
+        authDomain: "calisto-26845.firebaseapp.com",
+        projectId: "calisto-26845",
+        storageBucket: "calisto-26845.appspot.com",
+        messagingSenderId: "370374933586",
+        appId: "1:370374933586:web:7eb6215470a0588e7f738b"
+      }
+};
