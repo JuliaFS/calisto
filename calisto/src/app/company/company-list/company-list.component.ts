9764c95fda1a2e7d226642cde2cdad28ded9367e
Yulia <yuliya.f.s@gmail.com>
2024-04-01 01:00:21 +0300

    aded css to company list component

--- calisto/src/app/company/company-list/company-list.component.ts
+++ calisto/src/app/company/company-list/company-list.component.ts
@@ -1,57 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { DataService } from 'src/app/shared/data.service';
-// import { map } from 'rxjs/operators';
-// import { Company } from 'src/app/model/company';
-
-// @Component({
-//   selector: 'app-company-list',
-//   templateUrl: './company-list.component.html',
-//   styleUrls: ['./company-list.component.css']
-// })
-// export class CompanyListComponent implements OnInit {
-//   companies?: Company[];
-//   currentCompany?: Company;
-//   //userId : string | null = localStorage.getItem('userId');
-//   currentIndex = -1;
-//   title = '';
-
-//   constructor(private data: DataService) { }
-
-//   ngOnInit(): void {
-//     this.retrieveCompanies();
-//     //console.log(this.userId);
-//     //console.log(this.companies)
-//   }
-
-//   refreshList(): void {
-//     this.currentCompany = undefined;
-//     this.currentIndex = -1;
-//     this.retrieveCompanies();
-//   }
-
-//   retrieveCompanies(): void {
-//     this.data.getAll().snapshotChanges().pipe(
-//       map(changes =>
-//         changes.map(c =>
-//           ({ id: c.payload.doc.id, ...c.payload.doc.data() })
-//         )
-//       )
-//     ).subscribe(data => {
-//       this.companies = { ...data };
-//       //this.companies.userId = this.userId;
-//     });
-//   }
-
-//   // setActiveCompany(company: Company, index: number): void {
-//   //   this.currentCompany = company;
-//   //   this.currentIndex = index;
-//   // }
-
-//   setActiveCompany(company: Company): void {
-//     this.currentCompany = company;
-//   }
-// }
-
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Company } from 'src/app/model/company';
@@ -75,40 +21,11 @@ export class CompanyListComponent implements OnInit{
 
   constructor( private auth: AuthService, private data: DataService, private router: Router){}
 
-  //companyList: Observable<Company[]> = []
-
-
   ngOnInit(): void {
-  //   this.data.getAllCompanies().subscribe({
-  //     next(response) { this.companyList = response.map(e => e.payload.doc.data()); },
-  //     error(err) { console.error('Error: ' + err); },
-  //     complete() { console.log('Completed'); }
-  // });
-  this.getAllCompanies();
-    
+    this.getAllCompanies();
   }
 
   getAllCompanies(){
-
-  // const company$ = this.data.getAllCompanies().snapshotChanges().pipe(map((companies) =>companies.map(compani=>{
-  //   const data = s.payload.doc.id;
-  //   console.log(data);
-  //   return data;
-  // })));
-
-// -------
-// schoolProductCollectionFetch(schoolName){
-    
-//   const school = this.db.collection(schoolName)
-//   this.school$ = school.snapshotChanges().pipe(map(schoollist=>schoollist.map(s=>{
-//     const data = s.payload.doc.id;
-//     console.log(data);
-//     return data;
-//   })));
-//   return this.school$
-
-//  }
-//  -----
 
     this.data.getAllCompanies().subscribe( res => {
       this.companyList = res.map((e : any) => {
@@ -121,22 +38,4 @@ export class CompanyListComponent implements OnInit{
       alert('Error while fetching company data!');
     })
   }
-
-  // companyDetails(x: Company){
-  //   // const companyData = this.data.getOneCompany(x);
-  //   // console.log('company details in company list: ' + {companyData })
-  //   // //this.router.navigate(['/company/company-details', companyData]);
-  //   // this.router.navigate(['/company/company-details']);
-  // }
-
-  // deleteCompany(x: Company){
-  //   this.data.deleteCompany(x);
-  // }
-
-  // // getOne(x: Company){
-  // //   const test = this.data.getOneCompany(x);
-  // //   console.log(x);
-  // //   return test;
-  // // }
-
   }
