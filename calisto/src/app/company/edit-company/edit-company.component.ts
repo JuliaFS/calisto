5f83f55d65891a6832cc2c673c481ce284158923
Yulia <yuliya.f.s@gmail.com>
2024-04-01 14:20:02 +0300

    add error to comany details form

--- calisto/src/app/company/edit-company/edit-company.component.ts
+++ calisto/src/app/company/edit-company/edit-company.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Company } from 'src/app/model/company';
 import { DataService } from 'src/app/shared/data.service';
 import { Location } from '@angular/common';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-company',
@@ -41,6 +42,11 @@ export class EditCompanyComponent implements OnInit{
   }
 
   updateCompany(): void {
+    if(this.currentCompany.name == '' || this.currentCompany.country == ''
+      || this.currentCompany.working_capital < 1 || this.currentCompany.invested_capital < 1){
+        this.message = "Pls enter correct data!";
+        return;
+      }
     const data = {
       name: this.currentCompany.name,
       country: this.currentCompany.country,
@@ -55,4 +61,62 @@ export class EditCompanyComponent implements OnInit{
     this.location.back();
   }
 }
+
+// import { Component, Input, OnInit } from '@angular/core';
+// import { ActivatedRoute, Router } from '@angular/router';
+// import { Company } from 'src/app/model/company';
+// import { DataService } from 'src/app/shared/data.service';
+// import { Location } from '@angular/common';
+
+// @Component({
+//   selector: 'app-edit-company',
+//   templateUrl: './edit-company.component.html',
+//   styleUrls: ['./edit-company.component.css']
+// })
+// export class EditCompanyComponent implements OnInit{
+//   companyId : any = this.act.snapshot.paramMap.get('id');
+
+//   currentCompany : any = {
+//     name: '',
+//     country: '',
+//     working_capital: 0,
+//     invested_capital: 0,
+//   };
+
+//   message = '';
+
+//   constructor(
+//     private data: DataService, 
+//     private act: ActivatedRoute, 
+//     private router: Router, 
+//     private location: Location
+//   ){}
+
+//    ngOnInit(): void {
+//     this.data.getCompanyById(this.companyId).subscribe( (data) => {
+//       //console.log('currentCompany before: ' + this.currentCompany);
+//       this.currentCompany = data;
+//       //console.log('currentCompany after: ' + this.currentCompany);
+//     });
+// }
+
+//   ngOnChanges(): void {
+//     this.message = '';
+//   }
+
+//   updateCompany(): void {
+//     const data = {
+//       name: this.currentCompany.name,
+//       country: this.currentCompany.country,
+//       working_capital: this.currentCompany.working_capital,
+//       invested_capital: this.currentCompany.invested_capital,
+//     };
+
+//     this.data.update(this.companyId, data)
+//         .then(() => this.message = 'The company was updated successfully!')
+//         .catch(err => console.log(err));
+        
+//     this.location.back();
+//   }
+// }
 
